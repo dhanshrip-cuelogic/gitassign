e27ba41aba2ab5e5963d012b05a35301107ac6b1
dhanshrip35 <dhanshri.pawar@cuelogic.com>
2019-12-19 17:03:24 +0530

    created addition file

---
+++ addition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class addition
+{
+	static int add(int a, int b)
+	{
+		return (a+b);
+	}
+
+	public static void main(String args[])
+	{
+		int sum=add(10,20);
+		System.out.println(sum);
+		
+	}
+} 
+
