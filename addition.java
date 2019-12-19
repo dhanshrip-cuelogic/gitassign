88c4fa955c912548bcebb1262725ec284af3969c
dhanshrip35 <dhanshri.pawar@cuelogic.com>
2019-12-19 17:24:14 +0530

    added mul method in addition file

--- addition.java
+++ addition.java
@@ -7,10 +7,18 @@ class addition
 		return (a+b);
 	}
 
+	static int mul(int a, int b)
+	{
+		return (a*b);
+	}
+
 	public static void main(String args[])
 	{
 		int sum=add(10,20);
 		System.out.println(sum);
+
+		int mult=mul(10,20);
+		System.out.println("MULTPLICATION :- "mult);
 		
 	}
 } 
