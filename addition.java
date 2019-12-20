a8fb0709bb7d440c8488308815eb359f99d16263
dhanshrip35 <dhanshri.pawar@cuelogic.com>
2019-12-20 10:50:57 +0530

    added division method in addition file

--- addition.java
+++ addition.java
@@ -7,10 +7,17 @@ class addition
 		return (a+b);
 	}
 
+	static int div(int a, int b)
+	{
+		return (a/b);
+	}
 	public static void main(String args[])
 	{
 		int sum=add(10,20);
 		System.out.println(sum);
+
+		int divs=div(10,20);
+		System.out.println(divs);
 		
 	}
 } 
